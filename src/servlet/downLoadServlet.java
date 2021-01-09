56f0bc25e5ff51d8ef4688ae0eb012b5ec3db0b7
sxse <huangrenjier84@gmail.com>
2021-01-09 23:44:40 +0800

    实现文件下载

--- src/servlet/downLoadServlet.java
+++ src/servlet/downLoadServlet.java
@@ -2,12 +2,14 @@ package servlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletOutputStream;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.FileInputStream;
 import java.io.IOException;
 
+@WebServlet("/photoDL")
 public class downLoadServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
